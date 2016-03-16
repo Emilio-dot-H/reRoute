4e7b3f58a81cbcdfa37c26a24cd6b9bf936ae730
Emilio-dot-H <emiliohajj@hotmail.com>
2016-03-16 13:17:07 -0400

    I made a getDistance() method that returns the distance between intersection a and b

--- cas2xb3/group40/Intersection.java
+++ cas2xb3/group40/Intersection.java
@@ -2,7 +2,8 @@ package cas2xb3.group40;
 
 import java.util.ArrayList;
 
-public class Intersection {
+
+public class Intersection{
 
     private static int intersectionNum = 0;
 
@@ -18,12 +19,20 @@ public class Intersection {
 	}
 
 	public double getX() {
-        return x;
+        return x; //
 	}
 
 	public double getY() {
         return y;
     }
+	public double getDistance(Intersection a, Intersection b){ //returns distance between 2 intersections
+		double x1 = a.getX(); //x coordinate of the first intersection (Intersection a)
+		double y1 = a.getY(); //y coordinate of the first intersection (Intersection a)
+		double x2 = b.getX(); //x coordinate of the second intersection (Intersection b)
+		double y2 = b.getY(); //y coordinate of the second intersection (Intersection b) 
+		double dist_pyth = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+		return dist_pyth;
+	}
 
 	public String getStreets() {
 		return this.streets;
@@ -32,6 +41,12 @@ public class Intersection {
 	public int getId() {
 		return this.id;
 	}
+
+	public String toString() {
+		return "[" + streets + "]";
+	}
+
+}
 
 	public String toString() {
 		return "[" + streets + "]";
